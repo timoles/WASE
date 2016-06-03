e27e61b544a5039741096eb3e16efdb6707bb9ef
Thomas Patzke <thomas@patzke.org>
2016-06-04 00:13:04 +0200

    Code cleanup in WASEHTMLParser and Py2/3 compatibility

--- WASEHTMLParser.py
+++ WASEHTMLParser.py
@@ -1,20 +1,12 @@
-from HTMLParser import HTMLParser
-from pprint import PrettyPrinter
-
-pp = PrettyPrinter(indent=4)
+import sys
+if sys.version_info[0] == 2:
+    from HTMLParser import HTMLParser
+else:
+    from html.parser import HTMLParser
 
 # extract values from attrList of attributes whose name is contained in attrNames
 def add_attrs(attrNames, attrList):
     return [a[1] for a in filter(lambda attr: attr[0] in attrNames, attrList)]
-
-def has_attr(attrs, attr):
-    return attr in map(lambda kv: kv[0], attrs)
-
-def attr_val_is(attrs, attr, val):
-    try:
-        return filter(lambda kv: kv[0] == attr, attrs)[0][1] == val
-    except:
-        return False
 
 class WASEHTMLParser(HTMLParser, object):
     def reset(self):
@@ -36,33 +28,33 @@ class WASEHTMLParser(HTMLParser, object):
 
     def handle_starttag(self, tag, attrs):
         if tag == "iframe":
-            self.frames.update(add_attrs(["src"], attrs))
+            self.frames = self.frames.union(add_attrs(["src"], attrs))
         elif tag == "base":
-            self.base.update(add_attrs(["href"], attrs))
-        elif tag == "link" and attr_val_is(attrs, "rel", "stylesheet"):
-            self.stylesheets.update(add_attrs(["href"], attrs))
+            self.base = self.base.union(add_attrs(["href"], attrs))
+        elif tag == "link" and "rel" in attrs and attrs["rel"] == "stylesheet":
+            self.stylesheets = self.stylesheets.union(add_attrs(["href"], attrs))
         elif tag == "script":
-            self.scripts.update(add_attrs(["src"], attrs))
+            self.scripts = self.scripts.union(add_attrs(["src"], attrs))
         elif tag == "a" or tag == "area":
-            self.links.update(add_attrs(["href"], attrs))
+            self.links = self.links.union(add_attrs(["href"], attrs))
         elif tag == "img" or tag == "input":
-            self.images.update(add_attrs(["src"], attrs))
+            self.images = self.images.union(add_attrs(["src"], attrs))
         elif tag == "svg" or tag == "image":
-            self.images.update(add_attrs(["href", "xlink:href"], attrs))
+            self.images = self.images.union(add_attrs(["href", "xlink:href"], attrs))
         elif tag == "audio":
-            self.audio.update(add_attrs(["src"], attrs))
+            self.audio = self.audio.union(add_attrs(["src"], attrs))
         elif tag == "video":
-            self.video.update(add_attrs(["src"], attrs))
+            self.video = self.video.union(add_attrs(["src"], attrs))
         elif tag == "object":
-            self.objects.update(add_attrs(["data"], attrs))
+            self.objects = self.objects.union(add_attrs(["data"], attrs))
         elif tag == "embed":
-            self.objects.update(add_attrs(["src"], attrs))
+            self.objects = self.objects.union(add_attrs(["src"], attrs))
         elif tag == "applet":
-            self.objects.update(add_attrs(["code"], attrs))
+            self.objects = self.objects.union(add_attrs(["code"], attrs))
         elif tag == "form":
-            self.formactions.update(add_attrs(["action"], attrs))
+            self.formactions = self.formactions.union(add_attrs(["action"], attrs))
         elif tag == "input" or tag == "button":
-            self.formactions.update(add_attrs(["formaction"], attrs))
+            self.formactions = self.formactions.union(add_attrs(["formaction"], attrs))
         else:
             return
 
