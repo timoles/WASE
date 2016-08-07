82a0511968bd0081877420eaafa4c504e4f3f9ad
Thomas Patzke <thomas@patzke.org>
2016-08-07 23:18:28 +0200

    Added search function to perform arbitrary queries

--- WASEQuery.py
+++ WASEQuery.py
@@ -10,6 +10,10 @@ def print_debug(*arglist):
         print(file=sys.stderr, *arglist)
 
 ### Query Subcommands ###
+def add_default_aggregation(s):
+    a = A("terms", field="request.url.raw")
+    s.aggs.bucket("urls", a)
+
 def query_missing(s, field, name, methods=None, responsecodes=None, invert=False):
     # main query
     q = Q("match", ** { field: name })
@@ -31,8 +35,7 @@ def query_missing(s, field, name, methods=None, responsecodes=None, invert=False
                 s = s.filter("term", ** { 'response.status': rc })
 
     # aggregate
-    a = A("terms", field="request.url.raw")
-    s.aggs.bucket("urls", a)
+    add_default_aggregation(s)
 
     print_debug(s.to_dict())
     return s
@@ -45,27 +48,35 @@ def query_missingparam(s, paramname, methods=None, responsecodes=None, invert=Fa
     s = query_missing(s, 'request.parameternames', paramname, methods, responsecodes, invert)
     return s.execute()
 
+def query(s, q):
+    s.query = Q("query_string", query=q)
+    #add_default_aggregation(s)
+    print_debug(s.to_dict())
+    return s.execute()
+
 ### Main ###
 argparser = argparse.ArgumentParser(description="WASE Query Tool")
 argparser.add_argument("--server", "-s", action="append", default="localhost", help="ElasticSearch server")
 argparser.add_argument("--index", "-i", default="wase-*", help="ElasticSearch index pattern to query")
 argparser.add_argument("--fields", "-f", action="append", help="Add fields to output. Prints full result instead of aggregated URLs.")
-argparser.add_argument("--method", "-m", action="append", help="Restrict search to given methods")
-argparser.add_argument("--responsecode", "-c", action="append", help="Restrict search to responses with the given codes. Can be a single code (e.g. 200), a range (200-299) or wildcard (2*)")
 argparser.add_argument("--debug", "-d", action="store_true", help="Debugging output")
 subargparsers = argparser.add_subparsers(title="Query Commands", dest="cmd")
 
 argparser_missingheader = subargparsers.add_parser("missingheader", help="Search for URLs which responses are missing a header")
 argparser_missingheader.add_argument("header", help="Name of the header")
 argparser_missingheader.add_argument("--invert", "-n", action="store_true", help="Invert result, list all URLs where header is set")
+argparser_missingheader.add_argument("--method", "-m", action="append", help="Restrict search to given methods")
+argparser_missingheader.add_argument("--responsecode", "-c", action="append", help="Restrict search to responses with the given codes. Can be a single code (e.g. 200), a range (200-299) or wildcard (2*)")
 
 argparser_missingparam = subargparsers.add_parser("missingparameter", help="Search for URLs where the requests are missing a parameter with the given name")
 argparser_missingparam.add_argument("parameter", help="Name of parameter to search")
 argparser_missingparam.add_argument("--invert", "-n", action="store_true", help="Invert result, list all URLs where header is set")
+argparser_missingparam.add_argument("--method", "-m", action="append", help="Restrict search to given methods")
+argparser_missingparam.add_argument("--responsecode", "-c", action="append", help="Restrict search to responses with the given codes. Can be a single code (e.g. 200), a range (200-299) or wildcard (2*)")
 #argparser_missingparam.add_argument("--type", "-t", choices=["url", "body", "cookie", "xml", "xmlattr", "multipartattr", "json", "unknown"], help="Restrict search to given request parameter type")
 
-argparser_all = subargparsers.add_parser("search", help="Make arbitrary queries")
-argparser_missingparam.add_argument("query", nargs="*", default=["*"], help="Query string")
+argparser_search = subargparsers.add_parser("search", help="Make arbitrary queries")
+argparser_search.add_argument("query", nargs="*", default=["*"], help="Query string")
 
 args = argparser.parse_args()
 print_debug(args)
@@ -78,8 +89,10 @@ if args.cmd == "missingheader":
     r = query_missingheader(s, args.header, args.method, args.responsecode, args.invert)
 elif args.cmd == "missingparameter":
     r = query_missingparam(s, args.parameter, args.method, args.responsecode, args.invert)
+elif args.cmd == "search":
+    r = query(s, " ".join(args.query))
 else:
-    print("This shouldn't happen!")
+    argparser.print_help()
     sys.exit(1)
 
 if not r:
